1a17748b36f6cd8d6f2fcd6b461ad48133eb90e6
ExquisiteCore <xiaolongqq1y@163.com>
2025-08-31 22:03:52 +0800

    add src/Version.h

---
+++ src/mod/Version.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#define LIB_NAME "Carpet-Mod-For-LL"
+
+#define LIB_VERSION_MAJOR      0
+#define LIB_VERSION_MINOR      0
+#define LIB_VERSION_REVISION   0
+#define LIB_VERSION_PRERELEASE ""
+#define LIB_VERSION_BUILD_META ""
+
+#define TARGET_PROTOCOL_VERSION 686
+
+#define LIB_AUTHOR     "ExquisiteCore"
+#define LIB_REPOSITORT "https://github.com/ExquisiteCore/Carpet-Mod-For-LL"
